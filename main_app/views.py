eaafa7bacfb671c2830303cc669823ddaa722046
bavila19 <brendaavila015@gmail.com>
2023-01-30 09:05:01 -0700

    Added templates for home & about. Updated views as well

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.views import View 
+# from django.views import View 
+from django.views.generic.base import TemplateView
 from django.http import HttpResponse
+
 # Create your views here.
 
 # Home class 
-class Home(View):
-    def get(self, request):
-        return HttpResponse ("Plant home")
+class Home(TemplateView):
+    template_name = "home.html"
+
 
 # Plant info class 
 
 # My plant class 
+
+# About 
+class About(TemplateView):
+    template_name= "about.html"
