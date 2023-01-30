e199a48ab14c38c40adf151daa837430dd2c87d0
bavila19 <brendaavila015@gmail.com>
2023-01-30 08:43:11 -0700

    Home Route working

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import View 
+from django.http import HttpResponse
 # Create your views here.
+
+# Home class 
+class Home(View):
+    def get(self, request):
+        return HttpResponse ("Plant home")
+
+# Plant info class 
+
+# My plant class 
