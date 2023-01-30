dac4ab96004af23955cb7171b6d1db808b0b98d2
bavila19 <brendaavila015@gmail.com>
2023-01-30 11:44:05 -0700

    Added static data and dummy data

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
-    path('plantinfo/', views.Plant_info.as_view(), name = "plant_info")
+    path('plantinfo/', views.PlantInfo.as_view(), name = "plant_info")
 ]
