918b289fd74c91ede1013fb72a12dd72fb960726
bavila19 <brendaavila015@gmail.com>
2023-02-03 08:58:41 -0700

    changing what shows up in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import MyPlant, Water # import the Artist model from models.py
+from .models import Plant # import the Artist model from models.py
 # Register your models here.
 
-admin.site.register( MyPlant)
-admin.site.register( Water)
+admin.site.register( Plant)
 
 # admin.site.register( Water) # this line will add the model to the admin panel
  # this line will add the model to the admin panel
